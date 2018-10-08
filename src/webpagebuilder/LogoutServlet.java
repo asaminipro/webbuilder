e2638ce37ef273cc33a6bae98549146b769aa0f8
asaminipro <42337966+asaminipro@users.noreply.github.com>
2018-10-08 12:18:25 +0530

    logout servelet code by sikha

---
+++ src/webpagebuilder/LogoutServlet.java
@@ -0,0 +1,54 @@
+package webpagebuilder;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		 response.setContentType("text/html");  
+         PrintWriter out=response.getWriter();  
+           
+         request.getRequestDispatcher("index.jsp").include(request, response);  
+           
+         HttpSession session=request.getSession();  
+         session.invalidate();  
+         
+         //out.print("You are successfully logged out!"); 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

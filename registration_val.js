33c739946d65ebeaf8f2a255e1907277eb5a761e
asaminipro <42337966+asaminipro@users.noreply.github.com>
2018-10-19 12:15:53 +0530

    by sikha

--- registration_val.js
+++ registration_val.js
@@ -3,9 +3,7 @@ function formValidation()
 var uid = document.registration.userid;
 var passid = document.registration.passid;
 var uname = document.registration.username;
-var uadd = document.registration.address;
-var ucountry = document.registration.country;
-var uzip = document.registration.zip;
+
 var uemail = document.registration.email;
 var umsex = document.registration.msex;
 var ufsex = document.registration.fsex; if(userid_validation(uid,5,12))
